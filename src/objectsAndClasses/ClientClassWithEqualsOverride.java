107ec2aa2c8284d409774c639e17698231be326a
ankit110 <ankitkr.1101@gmail.com>
2025-07-13 17:48:39 +0530

    Classes and Object practice

--- src/objectsAndClasses/ClientClassWithEqualsOverride.java
+++ src/objectsAndClasses/ClientClassWithEqualsOverride.java
@@ -9,6 +9,16 @@ public class ClientClassWithEqualsOverride {
 
     @Override
     public boolean equals(Object obj) {
+
+        if(this == obj)
+            return true;
+
+        if(obj == null)
+            return false;
+
+        if(getClass() != obj.getClass())
+            return false;
+
         ClientClassWithEqualsOverride other = (ClientClassWithEqualsOverride) obj;
 
         return id == other.id;
