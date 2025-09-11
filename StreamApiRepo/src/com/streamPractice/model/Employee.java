a3a09aeb84f77a180b7ae0c40b13f7d36311156f
Ankit kumar <ankitkr.1101@gmail.com>
2025-09-12 01:40:51 +0530

    Stream Api practice

---
+++ StreamApiRepo/src/com/streamPractice/model/Employee.java
@@ -0,0 +1,42 @@
+package com.streamPractice.model;
+
+public class Employee {
+    int id;
+    String name;
+    double salary;
+    String department;
+
+    public Employee(int id, String name, double salary, String department) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", salary=" + salary +
+            ", department='" + department + '\'' +
+            '}';
+    }
+
+}
