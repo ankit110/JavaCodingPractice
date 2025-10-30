27cd2446ff81c4691e933515f368a59295666144
Ankit kumar <ankitkr.1101@gmail.com>
2025-10-30 23:28:22 +0530

    dsa practice

---
+++ StreamApiRepo/src/com/interview/dsa/ReverseStringArray.java
@@ -0,0 +1,26 @@
+package com.interview.dsa;
+
+class ReverseStringArray {
+    public void reverseString(char[] s) {
+
+        if(s.length == 0) {
+            return;
+        }
+
+        int i = 0;
+        int j = s.length - 1;
+
+        while(i <= j) {
+            swap(s, i, j);
+            i++;
+            j--;
+        }
+
+    }
+
+    private void swap(char[] s, int i, int j) {
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
+}
